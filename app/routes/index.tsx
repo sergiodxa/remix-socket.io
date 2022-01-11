fe45de45043c91379295920b72e55214b454db83
Sergio Xalambrí <hello@sergiodxa.com>
2022-01-11 16:21:50 -0500

    Update index.tsx

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -13,6 +13,11 @@ export default function Index() {
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
       <h1>Welcome to Remix</h1>
+      <div>
+        <button onClick={() => socket.emit("something", "ping")}>
+          Send ping
+        </button>
+      </div>
       <ul>
         <li>
           <a
